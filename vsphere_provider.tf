55cb8c76bcb1e6dce584c07a201f8d4c77824fb7
Sergey Marunich <marunich.s@gmail.com>
2020-03-26 10:56:19 -0400

    Update vsphere_provider.tf

--- vsphere_provider.tf
+++ vsphere_provider.tf
@@ -1,8 +1,6 @@
 provider "vsphere" {
-  version = "1.15.0"
   user           = var.vsphere_user
   password       = var.vsphere_password
   vsphere_server = var.vsphere_server
-  # If you have a self-signed cert
   allow_unverified_ssl = true
 }
