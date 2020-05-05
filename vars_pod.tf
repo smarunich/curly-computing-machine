dfd48a6c4ef14b368581b80ca15a615c87eb6da5
Sergey Marunich <marunich.s@gmail.com>
2020-05-05 16:16:51 -0400

    Upgrade to 18.2.8, correction to resolution

--- vars_pod.tf
+++ vars_pod.tf
@@ -29,7 +29,7 @@ variable "controller" {
     cpu = 8
     memory = 24768
     disk = 128
-    template = "controller-18.2.6-9134-template"
+    template = "controller-18.2.8-9205-template"
   # mgmt_ip = ""
   # mgmt_mask = ""
   # default_gw = ""
